d2fce2054948b9c2dc16cf274a82770589a26c8d
joshua <wanhoyinjoshua@yahoo.comgit config --global user.name wanhoyinjoshuagit config --global user.name wanhoyinjoshua>
2023-08-27 10:56:05 +1000

    first commit

---
+++ app/components/styleparent.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+interface StyledTextContainerProps {
+ 
+  children:any
+}
+
+const StyledTextContainer: React.FC<StyledTextContainerProps> = ({ children }) => {
+  // Split the content into words
+  if(typeof children === 'number'){
+    return <div>{children}</div>;
+
+  }
+  const words = children.split(' ');
+  const stylingWord=["without a walking aid","brace","another"]
+  const combinedPattern = stylingWord.map(phrase => `(${phrase.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')})`).join('|');
+  const regex = new RegExp(combinedPattern, 'g');
+  const replacedContent = children.replace(regex, '<span class="text-red-500">$&</span>');
+
+  console.log(words)
+
+ 
+      
+      return <div dangerouslySetInnerHTML={{ __html: replacedContent }} />;
+     
+   
+};
+
+export default StyledTextContainer;
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,186 @@
+"use client"
 import Image from 'next/image'
-
+import { useState,useEffect } from 'react'
+import { start } from 'repl'
+import StyledTextContainer from './components/styleparent'
 export default function Home() {
+  const [startForm,setForm]= useState(false)
+  const [data,setData]= useState(null)
+  const [finishform,setfinishform]=useState(false)
+  const[possiblevalues,setPossibleValues]= useState([0,20])
+  const fetchData = async () => {
+    try {
+      const response = await fetch('/api/hello');
+      const jsonData = await response.json();
+      setData(jsonData.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+  useEffect(() => {
+   
+
+    fetchData();
+  }, []);
+  function loadForm(){
+    setForm(true)
+
+    
+    
+
+
+  }
+  interface BinaryTreeNode {
+    value: number;
+    left: BinaryTreeNode | null;
+    right: BinaryTreeNode | null;
+  }
+  
+  function getLeafNodeValues(node: BinaryTreeNode | null): number[] {
+    if (node === null) {
+      return [];
+    }
+    
+    if (node.left === null && node.right === null) {
+      return [node.value];
+    }
+    
+    const leftValues = getLeafNodeValues(node.left);
+    const rightValues = getLeafNodeValues(node.right);
+    
+    return [...leftValues, ...rightValues];
+  }
+ 
+  
+function left(){
+  if(data&& typeof data["left"]["value"] ==='number'){
+    {data&&setData(data["left"]);}
+    setfinishform(true)
+
+  }
+  else{
+    {data&&setData(data["left"]);}
+     
+  const leafNodeValues = getLeafNodeValues(data);
+  const uniqueSet = new Set(leafNodeValues);
+
+  // Convert the Set back to an array
+  const uniqueList = Array.from(uniqueSet);
+  setPossibleValues(uniqueList)
+
+  
+  console.log(uniqueList); // Output: [1, 2, 3, 4, 5]
+  
+  
+  }
+  
+  
+
+}
+function right(){
+  if(data&& typeof data["right"]["value"] ==='number'){
+    {data&&setData(data["right"]);}
+    setfinishform(true)
+
+  }
+  else{
+    {data&&setData(data["right"]);}
+    const leafNodeValues = getLeafNodeValues(data);
+    const uniqueSet = new Set(leafNodeValues);
+
+// Convert the Set back to an array
+const uniqueList = Array.from(uniqueSet);
+setPossibleValues(uniqueList)
+  
+
+console.log(uniqueList); // Output: [1, 2, 3, 4, 5]
+
+    
+    
+  }
+}
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+   <section>
+    {/*
+  Heads up! 👋
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+  Plugins:
+    - @tailwindcss/forms
+*/}
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+<div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
+  <div className="mx-auto max-w-lg">
+    <h1 className="text-center text-2xl font-bold text-indigo-600 sm:text-3xl">
+      WISCI Outcome Measure Tool
+    </h1>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    <p className="mx-auto mt-4 max-w-md text-center text-gray-500">
+      Desscription/ Rationale of the outcome measure, why this online tool will be useful..
+    </p>
+    {startForm==false? <section
+     
+      className="mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8"
+    >
+      <button
+        type="submit"
+        className="block w-full rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white"
+        onClick={()=>{loadForm()}}
+      >
+        Start outcome measure
+      </button>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+    </section>:
+    <section
+      
+      className="mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8"
+    >
+      <p className="text-center text-lg font-medium">
+        {finishform==false?<section>{Math.min(...possiblevalues)}-{Math.max(...possiblevalues)}</section>:""}
+      <StyledTextContainer>
+      {data&&data['value']}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+</StyledTextContainer>
+        
+        
+      
+      </p>
+      
+{finishform==false?<section>
+  <button
+        type="submit"
+        className="block w-full rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white"
+        onClick={left}
+      >
+        
+        {data&&data['left_q']!=null?<div>{data['left_q']}</div>:<div>yes</div>}
+      </button>
+      <br></br>
+      <button
+        type="submit"
+        className="block w-full rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white"
+        onClick={right}
+      >
+
+        {data&&data['right_q']!=null?<div>{data['right_q']}</div>:<div>no</div>}
+      </button>
+
+</section>:<section>
+
+  <div>The form is finshed try again</div>
+  
+  
+  </section>}
+
+      
+
+     
+    </section>
+    
+    }
+
+    
+  </div>
+</div>
+   </section>
   )
 }
