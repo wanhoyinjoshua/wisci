40e99efa3f41fd4f3bb4ff358962a6eee70e9db3
joshua <wanhoyinjoshua@yahoo.comgit config --global user.name wanhoyinjoshuagit config --global user.name wanhoyinjoshua>
2024-08-31 16:24:57 +1000

    hi

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,9 +4,9 @@ import Image from 'next/image'
 const Header = () => {
   
   return (
-    <div  className='h-full bg-slate-50 px-3 py-0 shadow-md'>
+    <a href='https://www.wisci.org/' className='h-full bg-slate-50 px-3 py-0 shadow-md'>
     <Image alt='logo' src="/Picture1.svg" height={100} width={190} />
-   </div>
+   </a>
   )
 }
 
