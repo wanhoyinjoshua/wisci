694837706796dbcee466f7264afe5cf91d8d4c30
joshua <wanhoyinjoshua@yahoo.comgit config --global user.name wanhoyinjoshuagit config --global user.name wanhoyinjoshua>
2024-08-31 16:49:39 +1000

    hi

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Walking Index for Spinal Cord Injury (WISCI) The Self-Report Version',
+  title: 'Walking Index for Spinal Cord Injury (WISCI) The Self-Report Version.',
   description: 'This self-report outcome measure scores your ability to walk. It has been tested for reliability and validity (against the original WISCI II version – Ditunno et al 2001). Self-report WISCI developed by: Marsha Ben, Lisa Harvey and Joanne Glinsky, University of Sydney, Australia (Ref: Ben M et al (2023) Spinal Cord)',
   
 }
